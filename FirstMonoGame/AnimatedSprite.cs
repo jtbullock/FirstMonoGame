717dce4fe5664b6512e45ac8c96f574c33684811
Josh Bullock <mail@joshb.me>
2021-01-11 09:47:33 -0500

    Initial commit

---
+++ FirstMonoGame/AnimatedSprite.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace FirstMonoGame
+{
+    public class AnimatedSprite
+    {
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private int _currentFrame;
+        private int _totalFrames;
+
+        public AnimatedSprite(Texture2D texture, int rows, int columns)
+        {
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            _currentFrame = 0;
+            _totalFrames = Rows * Columns;
+        }
+
+        public void Update()
+        {
+            _currentFrame++;
+            if(_currentFrame == _totalFrames)
+            {
+                _currentFrame = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = Texture.Width / Columns;
+            int height = Texture.Height / Rows;
+            int row = (int)(_currentFrame / (float)Columns);
+            int column = _currentFrame % Columns;
+
+            var sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            var destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
---
+++ FirstMonoGame/Game1.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace FirstMonoGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D background;
+        private Texture2D shuttle;
+        private Texture2D earth;
+        private Texture2D boi;
+
+        private AnimatedSprite animatedSprite;
+
+        private SpriteFont font;
+        private int score = 0;
+
+        private TimeSpan _lastTick = TimeSpan.FromSeconds(0);
+            
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            background = Content.Load<Texture2D>("stars");
+            shuttle = Content.Load<Texture2D>("shuttle");
+            earth = Content.Load<Texture2D>("earth");
+            boi = Content.Load<Texture2D>("images/boi_1");
+
+            font = Content.Load<SpriteFont>("Score");
+
+            var texture = Content.Load<Texture2D>("smiley_walk");
+            animatedSprite = new AnimatedSprite(texture, 4, 4);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (gameTime.TotalGameTime.TotalSeconds - _lastTick.TotalSeconds >= 1)
+            {
+                score++;
+                _lastTick = gameTime.TotalGameTime;
+            }
+
+            animatedSprite.Update();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
+            _spriteBatch.Draw(earth, new Vector2(400, 240), Color.White);
+            _spriteBatch.Draw(shuttle, new Vector2(450, 240), Color.White);
+            _spriteBatch.End();
+
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(font, $"Score: {score}", new Vector2(100, 100), Color.White);
+            _spriteBatch.End();
+
+            animatedSprite.Draw(_spriteBatch, new Vector2(400, 200));
+
+            base.Draw(gameTime);
+        }
+    }
+}
